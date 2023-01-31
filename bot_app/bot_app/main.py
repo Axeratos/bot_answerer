cb2e20876300ba5ebfb36241cdaab5b9c3307e3e
axeratos <nikitalukianitcares@gmail.com>
2023-01-31 13:11:56 +0200

    Get env variables from config

--- bot_app/bot_app/main.py
+++ bot_app/bot_app/main.py
@@ -1,21 +1,19 @@
 import asyncio
-import os
 import pathlib
 
 from aiogram import Bot, Dispatcher, Router
 from aiogram import types
 
-from dotenv import load_dotenv
+from db.db_queries import get_chat
 
-from bot_app.db.db_queries import get_chat
+from core import Config
 
-load_dotenv()
-
-bot = Bot(token=os.getenv("TOKEN"), parse_mode="HTML")
+bot = Bot(token=Config.bot.BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 router = Router()
 
 MEDIA_DIR = pathlib.Path(__file__).resolve().parent.parent.joinpath("admin_app/admin_panel/media")
+MEDIA_DIR1 = pathlib.Path(__file__).resolve().parent.parent.joinpath("media")
 
 
 @router.chat_join_request()
@@ -24,14 +22,16 @@ async def channel_join_request_handler(join_request: types.ChatJoinRequest):
 
 
 @router.channel_post()
-async def test(message: types.Message, context: dict):
+async def test(message: types.Message):
     url = message.chat.username
     chat_object = await get_chat(url)
-    image = types.FSInputFile(MEDIA_DIR.joinpath(chat_object.greeting_image))
+    image = types.FSInputFile(MEDIA_DIR1.joinpath(chat_object.greeting_image))
     await message.answer_photo(photo=image, caption=chat_object.greeting_text)
 
 
 if __name__ == '__main__':
     dp.include_router(router)
     db_path = pathlib.Path(__file__).resolve().parent.joinpath("admin_panel/db.sqlite3")
-    asyncio.run(dp.start_polling(bot, context={"hello": "user"}))
+    print("[INFO] Bot started [INFO]")
+    print(Config.db.POSTGRES_HOST)
+    asyncio.run(dp.start_polling(bot))
