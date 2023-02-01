ad3315d4c2ff9137f90300ee859a515f216fa76c
axeratos <nikitalukianitcares@gmail.com>
2023-02-01 21:26:23 +0200

    Allowed hosts for django app

--- admin_app/admin_panel/core/settings.py
+++ admin_app/admin_panel/core/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = "django-insecure-z$l*c%8gs%-d%a8nk=er79x3jb-$qxq5p-k#6hrukfo1nu%0o-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = [
+    "0.0.0.0",
+    "127.0.0.1",
+]
 
 
 # Application definition
