937c95d65fc8088c5190e4e098054c87213ababa
axeratos <nikitalukianitcares@gmail.com>
2023-02-01 11:12:27 +0200

    Path builder for model updated

--- admin_app/admin_panel/admin_panel_app/services/image_paths_builders.py
+++ admin_app/admin_panel/admin_panel_app/services/image_paths_builders.py
@@ -3,4 +3,4 @@ from uuid import uuid4
 
 def greeting_image_path(instance, filename: str):
     _, extension = filename.rsplit(".", maxsplit=1)
-    return f"greeting_images/{uuid4()}/picture.{extension}"
+    return f"greeting_images/chat/{uuid4()}/picture.{extension}"
